03bb373568d90adff7e5e44edca372a24116cb36
Midnight <luisgabrielle1026@gmail.com>
2023-09-29 23:48:25 +0800

    initial commit

---
+++ cmd/root.go
@@ -0,0 +1,62 @@
+/*
+Copyright © 2023 LUIS GABRIELLE PUTAN <luisgabrielle1026@gmail.com>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+const asciiArt = `
+
+██████╗  ██████╗ ███╗   ███╗ ██████╗ ██╗     ██╗████████╗███████╗
+██╔══██╗██╔═══██╗████╗ ████║██╔═══██╗██║     ██║╚══██╔══╝██╔════╝
+██████╔╝██║   ██║██╔████╔██║██║   ██║██║     ██║   ██║   █████╗  
+██╔═══╝ ██║   ██║██║╚██╔╝██║██║   ██║██║     ██║   ██║   ██╔══╝  
+██║     ╚██████╔╝██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║   ███████╗
+╚═╝      ╚═════╝ ╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝   ╚══════╝
+                                                                 
+
+
+`
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "PomoLite",
+	Short: "Lightweight CLI Pomodoro Timer",
+	Long: fmt.Sprintf(`%s PomoLite is a lightweight CLI Pomodoro application desgned for students to efficiently accomplish tasks and maximize their learning potential
+Examples usage:
+	
+.\PomoLite.exe start -m 30 -b 5 - starts a 30-minute Pomodoro timer with a 5-minute break
+
+Developed by Putan Luis Gabrielle`, asciiArt),
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.PomoLite.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
---
+++ cmd/start.go
@@ -0,0 +1,112 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/schollz/progressbar/v3"
+	"github.com/spf13/cobra"
+)
+
+var minutes int
+var breakMinutes int
+var workDuration time.Duration
+var breakDuration time.Duration
+
+var controlChan chan bool
+var pauseFlag bool
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start a Pomodoro Timer.",
+	Long: `Start a Pomodoro Timer.
+
+	FLAGS:
+	-m : minutes of work
+	-b : minutes of break`,
+	Run: func(cmd *cobra.Command, args []string) {
+		controlChan = make(chan bool)
+		defer close(controlChan)
+
+		for {
+			workDuration = time.Duration(minutes) * time.Minute
+			breakDuration = time.Duration(breakMinutes) * time.Minute
+
+			fmt.Printf("Starting Pomodoro Timer for %s with a break of %s\n", workDuration.String(), breakDuration.String())
+
+			workComplete := make(chan bool)
+			breakComplete := make(chan bool)
+
+			go func() {
+				countDownStart(workDuration, workComplete)
+				fmt.Println("\nWork completed. Take a break.")
+				close(workComplete)
+			}()
+
+			select {
+			case <-workComplete: // Work completed, move to break
+			}
+
+			go func() {
+				countDownStart(breakDuration, breakComplete)
+				fmt.Println("\nBreak completed. Back to work.")
+				close(breakComplete)
+			}()
+
+			select {
+			case <-breakComplete: // Break completed, repeat the cycle
+			}
+		}
+	},
+}
+
+func countDownStart(duration time.Duration, completionChan chan bool) {
+	bar := progressbar.Default(int64(duration.Seconds()))
+	ticker := time.NewTicker(time.Second)
+	pauseFlag = false
+	defer ticker.Stop()
+
+	for remaining := duration; remaining > 0; {
+		select {
+		case <-ticker.C:
+			if !pauseFlag {
+				remaining -= time.Second
+				bar.Add(1)
+			}
+		case <-controlChan:
+			pauseTimer()
+			fmt.Println("\nTimer paused. Press Enter to resume.")
+			waitForEnter()
+			resumeTimer()
+		}
+	}
+
+
+
+	completionChan <- true // Notify that the countdown is complete
+}
+
+func waitForEnter() {
+	// Wait for Enter without blocking
+	go func() {
+		var input string
+		fmt.Scanln(&input)
+		controlChan <- true // Notify that Enter is pressed
+	}()
+}
+
+func pauseTimer() {
+	pauseFlag = true
+}
+
+func resumeTimer() {
+	pauseFlag = false
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	startCmd.Flags().IntVarP(&minutes, "minutes", "m", 25, "minutes to work")
+	startCmd.Flags().IntVarP(&breakMinutes, "break", "b", 5, "minutes to take a break")
+}
