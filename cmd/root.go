6aee62839c17ae0f5d12aba5e8ac673e332263aa
Midnight <luisgabrielle1026@gmail.com>
2023-09-30 19:58:55 +0800

    added docs in cli help

--- cmd/root.go
+++ cmd/root.go
@@ -28,11 +28,21 @@ var rootCmd = &cobra.Command{
 	Use:   "PomoLite",
 	Short: "Lightweight CLI Pomodoro Timer",
 	Long: fmt.Sprintf(`%s PomoLite is a lightweight CLI Pomodoro application desgned for students to efficiently accomplish tasks and maximize their learning potential
-Examples usage:
-	
-.\PomoLite.exe start -m 30 -b 5 - starts a 30-minute Pomodoro timer with a 5-minute break
+########################################################################################
 
-Developed by Putan Luis Gabrielle`, asciiArt),
+Example usage:
+	
+.\PomoLite.exe start -m 30 -b 5 :: Starts a 30-minute Pomodoro timer with a 5-minute break
+
+To pause the Pomodoro Timer, you can use the Pause Break button on your keyboard located near your function keys.
+
+To unpause or resume the Pomodoro Timer, press any key.
+
+
+
+Developed by PUTAN LUIS GABRIELLE <luisgabrielle1026@gmail.com>
+
+#######################################################################################`, asciiArt),
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
